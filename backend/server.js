86cdf521f27822ffd32f3e65c547dfd9e5728000
koduri-lohith <koduri25lohith@gmail.com>
2026-09-04 18:22:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -114,6 +114,8 @@ else {
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`MediKiosk server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`MediKiosk server running on port ${PORT}`);
 });
